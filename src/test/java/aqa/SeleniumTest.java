0e32b7f14741cc15517d0d51ed720b8000f6f2fd
aqaacc <mralextb@gmail.com>
2024-01-28 17:40:47 +0100

    3nd commit: List of Menu WebElements were found and saved into + forEach

--- src/test/java/aqa/SeleniumTest.java
+++ src/test/java/aqa/SeleniumTest.java
@@ -10,12 +10,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class SeleniumTest {
     WebDriver driver;
 //    private final By REGISTRATION_FORM = By.id("registration");
 //    private final By FIRST_NAME = By.id("NAME");
-//    private final By REGISTRATION_BTN = By.xpath(".//span[@class='smart-net-banner__info-button-text']");
+    private final By MENU_ITEM = By.xpath(".//li[contains(@class, 'submenu-lvl1__list-item--has-child')]");
     private final By ACCEPT_COOKIES_BTN = By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll");
     private final String SECTION = "mobilais telefons samsung galaxy s24 ultra";
 
@@ -24,23 +25,22 @@ public class SeleniumTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://1a.lv");
-        //driver.findElement(REGISTRATION_BTN).click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
 
-        //find element
+        //0.find element
         WebElement acceptBtn = driver.findElement(ACCEPT_COOKIES_BTN);
         //click on the element
         acceptBtn.click();
 
-        /*
-
-        1. find all sections
-        2. find necessary section
-        3. click on it
-
-         */
+        //1. find all sections
+        List<WebElement> menuItems = driver.findElements(MENU_ITEM);
+        //2. find necessary section
+        for (WebElement el : menuItems) {
+            System.out.println(el);
+        }
+        //3. click on it
     }
 
     @AfterMethod(alwaysRun = true)
