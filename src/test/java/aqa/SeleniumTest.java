97c1ace5889771dd929af15f17efb5f603f97d50
aqaacc <mralextb@gmail.com>
2024-01-28 19:48:55 +0100

    6th commit (update): in forEach cycle we have found 'Datortehnika, preces birojam' (1a.lv) and clicked on it

--- src/test/java/aqa/SeleniumTest.java
+++ src/test/java/aqa/SeleniumTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.List;
 
@@ -45,7 +44,6 @@ public class SeleniumTest {
             if (we.getText().equals(SECTION)) {
                 wait.until(ExpectedConditions.elementToBeClickable(we));
                 we.click();
-
                 Assert.assertEquals(driver.getCurrentUrl(),expectedSitePage,
                         "User was not redirected to the page");
                 break;
