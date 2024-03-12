7d867648f9c1d5382330436b5c9e7e4dd6b5287b
aqaacc <mralextb@gmail.com>
2024-03-12 19:32:57 +0100

    PageObject: added 2 methods: findElement(), selectMenuItem()

--- src/test/java/pageObject/pages/HomePage.java
+++ src/test/java/pageObject/pages/HomePage.java
@@ -1,12 +1,20 @@
 package pageObject.pages;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageObject.BaseFunc;
+
+import java.util.List;
 
 public class HomePage {
     private BaseFunc baseFunc;
     private final By ACCEPT_COOKIES_BTN
             = By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll");
+    private final By MENU_ITEM = By.xpath(".//li/a[@class = 'submenu-lvl1__link' ]");
+    private final By MENU =
+            By.xpath(".//div[contains(@class, 'submenu-lvl1 submenu-lvl1--invisible submenu-lvl1--index')]");
 
     public HomePage(BaseFunc baseFunc){
         this.baseFunc = baseFunc;
@@ -14,5 +22,22 @@ public class HomePage {
 
     public void acceptCookies() {
         baseFunc.click(ACCEPT_COOKIES_BTN);
+    }
+
+    public void selectMenuItem(String menuItemName) {
+
+        WebElement menuBlock = baseFunc.findElement(MENU);
+        List<WebElement> items = menuBlock.findElements(MENU_ITEM);
+
+        boolean isSectionFound = false;
+        for (WebElement we : items) {
+//            String SECTION = "Datortehnika, preces birojam";
+            if (we.getText().equals(menuItemName)) {
+                isSectionFound = true;
+                we.click();
+                break;
+            }
+        }
+        Assertions.assertTrue(isSectionFound, "Menu item is not found" + menuItemName);
     }
 }
